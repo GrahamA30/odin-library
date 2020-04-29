bc27283f5ac0f0db51555c5661541b37ed777518
GrahamA30 <aj.graham17@gmail.com>
2020-04-29 18:41:09 -0400

    user can now add and display a new book

--- library.js
+++ library.js
@@ -22,11 +22,43 @@ function addBookToLibrary(){
 
     const newBook = new Book(author,title,pages,read);
     myLibrary.push(newBook);
+    render();
 }
 
 function render(){
+    const library = document.querySelector("#library");
+
+    library.innerHTML = "";
     myLibrary.forEach((book) => {
-        console.log(book.info());
+        const newBook = document.createElement("div");
+        newBook.className = "book";
+
+        const author = document.createElement("p");
+        author.className = "author";
+        author.textContent = `author: ${book.author}`;
+
+        const title = document.createElement("p");
+        title.className = "title";
+        title.textContent = `title: ${book.title}`;
+
+        const pages = document.createElement("p");
+        pages.className = "pages";
+        pages.textContent = `${book.pages} pages`;
+
+        const read = document.createElement("p");
+        read.className = "read";
+        read.textContent = book.read ? "read": "not read";
+
+        newBook.appendChild(author);
+        newBook.appendChild(title);
+        newBook.appendChild(pages);
+        newBook.appendChild(read);
+        library.appendChild(newBook);
+
     });
 }
+
+const addBook = document.querySelector('#add-book');
+
+addBook.addEventListener('click',addBookToLibrary);
 
