e46b42776447d925f107e93363d640f680e7da35
GrahamA30 <aj.graham17@gmail.com>
2020-04-29 01:23:01 -0400

    implemented addToLibrar() function and a rende() function

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -6,6 +8,25 @@ function Book(title, author, pages, read){
 }
 
 Book.prototype.info = function(){
-    return `${title} by ${author}, ${pages} pages, ${read ? "read": "not read yet"}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+}
+const book = new Book("author","title","pages","false");
+
+console.log(book.info());
+
+function addBookToLibrary(){
+    let author = prompt("Enter the Name of the Author");
+    let title = prompt("Enter the title of the book");
+    let pages = prompt("Enter the number of pages");
+    let read = prompt("Have you read the book? Enter true or false").toLowerCase() == "true" ? true: false;
+
+    const newBook = new Book(author,title,pages,read);
+    myLibrary.push(newBook);
+}
+
+function render(){
+    myLibrary.forEach((book) => {
+        console.log(book.info());
+    });
 }
 
